09b483353f2d7133ec6d5c28fdd59dcb928b0563
jymyte <jyrimy@gmail.com>
2020-11-26 14:22:13 +0200

    FadeIn/Out for transitions.
    Started working on comics.

---
+++ Assets/Scripts/GameController.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class GameController : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    public void plaal() {
+        SceneFader.instance.LoadScene("Gameplay");
+    }
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
---
+++ Assets/Scripts/SceneFader.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SceneFader : MonoBehaviour
+{
+    public static SceneFader instance;
+
+    [SerializeField]
+    private GameObject fadePanel;
+    [SerializeField]
+    private Animator fadeAnim;
+
+    void Awake()
+    {
+        MakeSingleton();
+    }
+
+    void MakeSingleton() {
+        if (instance != null) {
+            Destroy(gameObject);
+        } else {
+            instance = this;
+            DontDestroyOnLoad(gameObject);
+        }
+    }
+
+    public void LoadScene(string scene) {
+        StartCoroutine(FadeInOut(scene));
+    }
+
+    IEnumerator FadeInOut(string scene) {
+        fadePanel.SetActive(true);
+        fadeAnim.Play("FadeIn");
+        yield return StartCoroutine(SceneCoroutine.WaitForRealSeconds(1f));
+        SceneManager.LoadScene(scene);
+        fadeAnim.Play("FadeOut");
+        yield return StartCoroutine(SceneCoroutine.WaitForRealSeconds(.7f));
+        fadePanel.SetActive(false);
+    }
+}
