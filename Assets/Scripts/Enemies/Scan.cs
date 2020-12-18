04bcc06b886c40344a70897696052e9ac8c8cca5
jymyte <jyrimy@gmail.com>
2020-12-18 14:50:15 +0200

    buggy scan

--- Assets/Scripts/Enemies/Scan.cs
+++ Assets/Scripts/Enemies/Scan.cs
@@ -5,14 +5,46 @@ using UnityEngine;
 public class Scan : MonoBehaviour
 {
     [SerializeField]
-    private float turnTime, turnAngle, startingAngle;
-    private float speed;
+    private Vector3 turnAngle, startingAngle, targetAngle, speed, currentAngle;
+
+    [SerializeField]
+    private float turnTime, startWaitTime;
+    private float waitTime;
     
     private void Start() {
+        waitTime = startWaitTime;
         speed = turnAngle / turnTime;
+        startingAngle = transform.position;
+        targetAngle.y = startingAngle.y + turnAngle.y;
     }
 
     private void Update() {
-        
+        rotateTowards();
+
+        if ((transform.rotation.eulerAngles.y >= targetAngle.y && targetAngle.y > startingAngle.y) || (transform.rotation.eulerAngles.y <= startingAngle.y)) {
+            if (waitTime <= 0) {
+                changeTargetAngle();
+            } else {
+                waitTime -= Time.deltaTime;
+            }
+        }
+    }
+
+    private void changeTargetAngle() {
+        if (targetAngle == startingAngle) {
+            targetAngle.y = startingAngle.y + turnAngle.y;
+        } else {
+            targetAngle = startingAngle;
+        }
+    }
+
+    private void rotateTowards() {
+        if (targetAngle.y > startingAngle.y) {
+            currentAngle.y += Time.deltaTime * speed.y;
+        } else {
+            currentAngle.y -= Time.deltaTime * speed.y;
+        }
+
+        gameObject.transform.eulerAngles = currentAngle;
     }
 }
