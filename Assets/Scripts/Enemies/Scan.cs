64421945ee70a5b90242218ba8814ea2b5e5a9f4
Mikko Kropsu <mikko.kropsu@kaamosgames.com>
2020-12-18 22:23:11 +0200

    Scan with wait

--- Assets/Scripts/Enemies/Scan.cs
+++ Assets/Scripts/Enemies/Scan.cs
@@ -9,11 +9,57 @@ public class Scan : MonoBehaviour
     public float maxRotation = 90f;
     [Tooltip("0 = up, 90 = right etc")]
     public float startRotation = 0;
+    public float waitTime = 1;
+    public bool wait = false;
+
+    float prevY;
+
+    float ourTime = 0;
+    bool clockwise;
 
     void Update()
     {
-        float y = startRotation + maxRotation * Mathf.Sin(Time.time * speed);
+        if (wait)
+            return;
+
+        float y = startRotation + maxRotation * Mathf.Sin(ourTime * speed);
 
         transform.rotation = Quaternion.Euler(0f, y, 0f);
+
+        ourTime += Time.deltaTime;
+
+        CheckWait(y);
+
+        prevY = y;
+    }
+
+    private void CheckWait(float y)
+    {
+        if (prevY > y)
+        {
+            if (clockwise == true)
+                BeginWait();
+            else
+                clockwise = false;
+        }
+        else
+        {
+            if (clockwise == false)
+                BeginWait();
+            else
+                clockwise = true;
+        }
+    }
+
+    private void BeginWait()
+    {
+        wait = true;
+        clockwise = !clockwise;
+        Invoke(nameof(OnTimerElapse), waitTime);
+    }
+
+    void OnTimerElapse()
+    {
+        wait = false;
     }
 }
