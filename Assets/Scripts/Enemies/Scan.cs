793e99b4249ff041eacc79399e292429a54473e9
jymyte <jyrimy@gmail.com>
2020-12-18 12:31:46 +0200

    Made prefab of the patrolling enemy.

---
+++ Assets/Scripts/Enemies/Scan.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Scan : MonoBehaviour
+{
+    [SerializeField]
+    private float turnTime, turnAngle, startingAngle;
+    private float speed;
+    
+    private void Start() {
+        speed = turnAngle / turnTime;
+    }
+
+    private void Update() {
+        
+    }
+}
