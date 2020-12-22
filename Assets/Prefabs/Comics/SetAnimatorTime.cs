e17164c974fb8cb36d3ab8960ceb8bc8b53f7cd9
Mikko Kropsu <mikko.kropsu@kaamosgames.com>
2020-12-22 21:38:33 +0200

    Panel fade in controller + anim, set animator time script

---
+++ Assets/Prefabs/Comics/SetAnimatorTime.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Assertions;
+
+[RequireComponent(typeof(Animator))]
+public class SetAnimatorTime : MonoBehaviour
+{
+    [SerializeField] float speed = 1;
+
+    private void Awake()
+    {
+        var animator = GetComponent<Animator>();
+
+        Assert.IsNotNull(animator);
+
+
+        animator.speed = speed;
+    }
+}
+
